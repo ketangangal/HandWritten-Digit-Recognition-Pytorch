622772c23a59dece9d758338ab4be35064e1c88f
Ketan Gangal <Ketangangal98@gmail.com>
2021-11-01 10:43:20 +0530

    Replaced Custom Logger with Loguru

--- src/training.py
+++ src/training.py
@@ -4,29 +4,28 @@ import torch
 from src.utils.data_mgmt import load_dataset, create_loader, save_plot, save_model
 from src.utils.model import NeuralNetwork
 from src.utils.common import read_config
-from logger.logger import Logger
+from loguru import logger
 
 
-def training(config):
+def training(config=None):
     # Load Dataset and create Loader
-    logs.info(log_type='INFO', log_message='Training Started')
-    train_data, test_data = load_dataset(config=config)
-    logs.info(log_type='INFO', log_message='Checking and Fetching Data')
-    train_data_loader, test_data_loader = create_loader(train_data, test_data, config=config)
-    logs.info(log_type='INFO', log_message='Data Loaders Created')
+    logger.info('Training Started')
+    train_data, test_data = load_dataset(config=config, logger=logger)
+    logger.info('Checking and Fetching Data')
+    train_data_loader, test_data_loader = create_loader(train_data, test_data, config=config, logger=logger)
 
     # Model Creation
     model = NeuralNetwork(config['param']['input_size'], config['param']['output_size'])
-    logs.info(log_type='INFO', log_message=f'Model Created {model}')
+    logger.info(f'Model Created {model}')
 
     # Error and Optimization
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters())
-    logs.info(log_type='INFO', log_message='Loss Function and Optimizer Created')
+    logger.info('Loss Function and Optimizer Created')
 
     # Device Selection
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    logs.info(log_type='INFO', log_message='Checking and Fetching Data')
+    logger.info(f'Using {device} for training ')
 
     model.to(device)
     n_epoch = config['param']['n_epoch']
@@ -35,8 +34,7 @@ def training(config):
 
     # Forward and Backward pass
     for epoch in range(n_epoch):
-        print(f'Epoch {epoch + 1}/{n_epoch} ')
-        logs.info(log_type='INFO', log_message=f'Epoch {epoch + 1}/{n_epoch} ')
+        logger.info(f'Epoch {epoch + 1}/{n_epoch} ')
         for batch, data in enumerate(train_data_loader):
             x = data[0].to(device)
             y = data[1].to(device)
@@ -53,14 +51,11 @@ def training(config):
             acc_.append(accuracy)
 
             if batch % 100 == 0:
-                print(f'   Batch {batch} Loss {loss.item():.4f} Accuracy {accuracy:.4f}')
-                logs.info(log_type='INFO',
-                          log_message=f'   Batch {batch} Loss {loss.item():.4f} Accuracy {accuracy:.4f}')
+                logger.info(f'Batch {batch} Loss {loss.item():.4f} Accuracy {accuracy:.4f}')
 
     # Save plot into artifacts
     path = os.path.join(config['directory']['main'], config['directory']['plot'])
-    save_plot(loss_, acc_, 'plot', path)
-    logs.info(log_type='INFO', log_message='Plot Saved')
+    save_plot(loss_, acc_, 'plot', path, logger=logger)
 
     # Test on test data
     test_loss = 0
@@ -78,17 +73,15 @@ def training(config):
             total += 1
 
     # log Results
-    print(f'Test Loss : {test_loss / total:.4f} Test_accuracy : {test_accuracy / total:.4f}')
-    logs.info(log_type='INFO',
-              log_message=f'Test Loss : {test_loss / total:.4f} Test_accuracy : {test_accuracy / total:.4f}')
+    logger.info(f'Test Loss : {test_loss / total:.4f} Test_accuracy : {test_accuracy / total:.4f}')
 
     # Save Model
     path = os.path.join(config['directory']['main'], config['directory']['model_dir'])
-    save_model(model, 'model', path)
-    logs.info(log_type='INFO', log_message='Model Saved')
+    save_model(model, 'model', path, logger=logger)
 
 
 if __name__ == '__main__':
     config = read_config(r'C:\Users\ketan\Desktop\DeepLearning\HandWritten-Digit-Recognition-Pytorch\config.yaml')
-    logs = Logger(os.path.join(config['directory']['main'], config['directory']['logs']))
+    log_path = os.path.join(config['directory']['main'], config['directory']['logs'])
+    logger.add(log_path, format="[{time:YYYY-MM-DD HH:mm:ss.SSS} - {level} - {module} ] - {message}", level="DEBUG")
     training(config=config)
--- src/utils/data_mgmt.py
+++ src/utils/data_mgmt.py
@@ -7,8 +7,9 @@ import time
 import os
 
 
-def load_dataset(config):
-    path = os.path.join(config['directory']['main'],config['directory']['root_dir'])
+def load_dataset(config, logger):
+    logger.info('Loading Dataset {IF Data present use it else download}')
+    path = os.path.join(config['directory']['main'], config['directory']['root_dir'])
     train_data = datasets.MNIST(
         root=path,
         train=True,
@@ -24,17 +25,19 @@ def load_dataset(config):
         target_transform=None,
         download=True
     )
+    logger.info('Data Loading Successful')
     return train_data, test_data
 
 
-def create_loader(train_data, test_data, config):
-
+def create_loader(train_data, test_data, config, logger):
+    logger.info('Creating data Loader')
     train_data_loader = DataLoader(dataset=train_data,
                                    batch_size=config['param']['batch_size'],
                                    shuffle=config['param']['shuffle'])
 
     test_data_loader = DataLoader(dataset=test_data,
                                   batch_size=config['param']['batch_size'])
+    logger.info('Loaders Created and Returned')
     return train_data_loader, test_data_loader
 
 
@@ -49,16 +52,20 @@ def get_unique_filename(filename, typ):
         return None
 
 
-def save_plot(loss, acc, name, path):
+def save_plot(loss, acc, name, path, logger):
+    logger.info('Saving Plots')
     unique_name1 = get_unique_filename(name, typ='Plot')
     path_to_plot1 = os.path.join(path, unique_name1)
-    fig = pd.DataFrame(data={'Loss': loss,'Accuracy': acc}).plot()
+    fig = pd.DataFrame(data={'Loss': loss, 'Accuracy': acc}).plot()
     plt.xlabel('Accuracy')
     plt.ylabel('Loss')
     plt.savefig(path_to_plot1)
+    logger.info('Plots Saved')
 
 
-def save_model(model, name, path):
+def save_model(model, name, path, logger):
+    logger.info('Saving Model')
     unique_name = get_unique_filename(name, typ='Model')
     path_to_model = os.path.join(path, unique_name)
     torch.save(model, path_to_model)
+    logger.info('Model saved')
